600468fb3e2ddd71cdeaef7486a0383c93eb60c1
Mastertrap21 <madalin.danceanu@yahoo.com>
2023-07-31 23:47:54 +0200

    feat(main-webServerTest): Added PlantServiceTest project as Unit Test project for PlantService Project

---
+++ src/PlantServiceTest/PlantServiceTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PlantServiceTest;
+
+public class PlantServiceTest : TestCore.TestCoreTest
+{
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void PlantService_NotImplemented()
+    {
+        Assert.Fail("Not implemented");
+    }
+}
